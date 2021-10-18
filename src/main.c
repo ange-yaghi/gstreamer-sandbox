9d70f6668d7acf654e23363bff56a484d45d5063
Ange Yaghi <me@angeyaghi.com>
2021-10-17 20:57:49 -0400

    VertigoTV filter tutorial

--- src/main.c
+++ src/main.c
@@ -1,25 +1,76 @@
 #include <gst/gst.h>
 
 int main(int argc, char *argv[]) {
-    GstElement *pipeline;
+    GstElement *pipeline, *source, *sink, *filter, *video_convert;
     GstBus *bus;
     GstMessage *msg;
+    GstStateChangeReturn ret;
 
     gst_init(&argc, &argv);
 
-    pipeline =
-        gst_parse_launch
-        ("playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
-            NULL);
+    source = gst_element_factory_make("videotestsrc", "source");
+    filter = gst_element_factory_make("vertigotv", "filter");
+    sink = gst_element_factory_make("autovideosink", "sink");
+    video_convert = gst_element_factory_make("videoconvert", "videoconvert");
 
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    pipeline = gst_pipeline_new("test-pipeline");
+
+    if (pipeline == NULL || source == NULL || sink == NULL || filter == NULL || video_convert == NULL) {
+        g_printerr("Not all elements could be created.\n");
+        return -1;
+    }
+
+    gst_bin_add_many(GST_BIN(pipeline), source, sink, filter, video_convert, NULL);
+    if (gst_element_link(source, filter) != TRUE) {
+        g_printerr("Source could not be linked to filter.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    if (gst_element_link(filter, video_convert) != TRUE) {
+        g_printerr("Filter could not be linked to video_convert.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    if (gst_element_link(video_convert, sink) != TRUE) {
+        g_printerr("video_convert could not be linked to sink.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    g_object_set(source, "pattern", 0, NULL);
+
+    ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        g_printerr("Unable to set the pipeline to the playing state.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
 
     bus = gst_element_get_bus(pipeline);
-    msg =
-        gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
-            GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
     if (msg != NULL) {
+        GError *err;
+        gchar *debug_info;
+
+        switch (GST_MESSAGE_TYPE(msg)) {
+        case GST_MESSAGE_ERROR:
+            gst_message_parse_error(msg, &err, &debug_info);
+            g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
+            g_printerr("Debugging information: %s\n:", debug_info ? debug_info : "none");
+            g_clear_error(&err);
+            g_free(debug_info);
+            break;
+        case GST_MESSAGE_EOS:
+            g_print("Unexpected message received.\n");
+            break;
+        default:
+            g_printerr("Unexpected message received\n");
+            break;
+        }
+
         gst_message_unref(msg);
     }
 
